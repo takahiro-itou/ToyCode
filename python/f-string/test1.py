feff92dd42d1e1a5e1204c04c1963d7924c97320
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-03-06 20:43:02 +0900

    未定義変数のエラーテスト

--- python/f-string/test1.py
+++ python/f-string/test1.py
@@ -2,7 +2,19 @@
 
 hoge = 123
 
+# inputs に指定したテキストのうち {} で括られた部分は変数として置換を行う
 inputs = 'hoge は {hoge} です'
+print(inputs)
+
+f_inputs="f'{inputs}'".format(inputs = inputs,)
+print(f_inputs)
+
+text = eval(f_inputs)
+print(text)
+
+# 変数名 (fuga) が定義されていない時の挙動テスト
+# エラーになる予定
+inputs = 'fuga は {fuga} です'
 print(inputs)
 
 f_inputs="f'{inputs}'".format(inputs = inputs,)
