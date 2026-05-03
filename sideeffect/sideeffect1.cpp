0e518e2c9775f91833e3ab81b61a46d9318db4c0
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-05-03 10:57:09 +0900

    副作用完了点

---
+++ sideeffect/sideeffect1.cpp
@@ -0,0 +1,15 @@
+
+#if 0
+program=sideeffect1;
+g++ -Wall -O2 -o bin_${program} ${program}.cpp;
+exit 0;
+#endif
+
+#include    <iostream>
+
+int main(int argc, char * argv[])
+{
+    int i = 10;
+    printf("%d %d %d\n", i, i++, i);
+}
+
